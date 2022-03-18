4e9e21e99688c591491b32134a56a361e7a5a08e
Nurgun Ganbarova <nurgunganbarova@outlook.com>
2022-03-18 19:59:15 +0400

    Add project files.

---
+++ tasknum4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace tasknum4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 472998;
+            a=Math.Abs(a);
+            int reqem_sayi = 1;
+           
+            
+                if (a<100000)
+                {
+                while ((a /= 10) >= 1 && a < 10000)
+                    reqem_sayi++;
+                    Console.WriteLine($"{reqem_sayi} reqemlidir");
+                }
+
+                else
+                {
+                    Console.WriteLine("nezere alinmir");
+                }
+            
+
+            
+        }
+    }
+}
